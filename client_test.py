70ce29e68fa68377643e603b15942a62519177bd
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2025-09-24 17:47:36 +0800

    added support for streaming

--- client_test.py
+++ client_test.py
@@ -1,21 +1,29 @@
 from openai import OpenAI
 
-
 client = OpenAI(
     api_key='fake-api-key',
     base_url='http://localhost:8000'
 )
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": input("Enter a message: ")
-        }
-    ],
-    model="mock-gpt"
+# chat_completion = client.chat.completions.create(
+#     messages=[
+#         {
+#             "role": "user",
+#             "content": input("Enter a message: ")
+#         }
+#     ],
+#     model="mock-gpt"
+# )
+
+# print()
+# print("Response:")
+# print(chat_completion.choices[0].message.content)
+
+stream = client.chat.completions.create(
+    model="mock-gpt",
+    messages=[{"role": "user", "content": input("Enter a message: ")}],
+    stream=True,
 )
 
-print()
-print("Response:")
-print(chat_completion.choices[0].message.content)
+for chunk in stream:
+    print(chunk.choices[0].delta.content or "")
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from typing import List, Optional
 from pydantic import BaseModel
-import time
+import time, asyncio, json
 from fastapi import FastAPI
+from starlette.responses import StreamingResponse
 
 class ChatMessage(BaseModel):
     role: str
@@ -14,14 +15,33 @@ class ChatCompletionRequest(BaseModel):
     temperature: Optional[float] = 0.1
     stream: Optional[bool] = False
 
+async def _resp_async_generator(model: str, text_resp: str):
+    tokens = text_resp.split(" ")
+    
+    for i, token in enumerate(tokens):
+        chunk = {
+            "id": i,
+            "object": "chat.completion.chunk",
+            "created": time.time(),
+            "model": model,
+            "choices": [{"delta": {"content": token + " "}}]
+        }
+        yield f"data: {json.dumps(chunk)}\n\n"
+        await asyncio.sleep(1)
+    
+    yield "data: [DONE]\n\n"
+
 app = FastAPI(title='OpenAI Compatible API')
 
 @app.post("/chat/completions")
 async def chat_completions(request: ChatCompletionRequest):
-    if request.messages and request.messages[0].role == 'user':
+    if request.messages:
         response_content = "As a mock AI assistant, I received your message: " + request.messages[-1].content
     else:
         response_content = 'As a mock AI assistant, I can only echo messages. No previous messsages found.'
+    
+    if request.stream:
+        return StreamingResponse(_resp_async_generator(request.model, response_content), media_type="application/x-ndjson")
     
     return {
         "id": "1337",
