f605174cd29655b0707b7ca349364dbf62ac4f07
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2025-10-04 16:14:36 +0800

    added SmolLM lifecycle with lifespan event

--- src/main.py
+++ src/main.py
@@ -6,14 +6,22 @@ from .slm import SmolLM
 from .database.core import Base, engine
 from .api import add_middlewares, register_routes
 from .logging import configure_logging, LogLevels
+from contextlib import asynccontextmanager
 
 configure_logging(LogLevels.info)
 
-app = FastAPI(title='SelfServeAI')
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    if os.getenv('RESPONSE_MODE', 'mock') == 'slm':
+        SmolLM.load()
+    
+    yield
+    
+    SmolLM.model = None
+    SmolLM.tokenizer = None
+
+app = FastAPI(title='SelfServeAI', lifespan=lifespan)
 Base.metadata.create_all(bind=engine)
-if os.getenv('RESPONSE_MODE', 'mock') == 'slm':
-    SmolLM.load()
 
 add_middlewares(app)
 register_routes(app)
-
