7712a3d6d0e148b88a4263c4794ec086236c0cf9
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2025-10-08 16:34:05 +0800

    fixed some cors issues

--- src/api.py
+++ src/api.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from src.chatCompletion.controller import router as chat_router
 from src.middleware import AuthMiddleware
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ['*']
 
 def add_middlewares(app: FastAPI):
+    app.add_middleware(CORSMiddleware, allow_origins=origins, allow_methods=["*"], allow_headers=["*"])
     app.add_middleware(AuthMiddleware)
 
 def register_routes(app: FastAPI):
